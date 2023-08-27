bfdb4a85881b11e02dabadf3ad67cc7cab58f08d
BiBax68 <lionel.farias@wanadoo.fr>
2023-08-27 09:39:20 +0200

    Correction Final

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -62,16 +62,14 @@ function validate() {
 
       console.log(objectInfo);
 
-      const confirm = document.getElementById("confirmationMsg").style
+      const confirm = document.getElementById("confirmationMsg");
 
-      confirm.display = "flex";
-      confirm.justifyContent = "center";
-      confirm.alignItems = "center";
+      confirm.classList.add("displayflex");
 
       let formstyle = document.querySelector(".gamingForm");
       formstyle.style.display = "none";
     } else {
-      alert("Veuillez rentrer des données corrects.")
+
     }
   })
 
@@ -84,9 +82,9 @@ function validationregex(form) {
   const date_regex = /^[0-9]{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])/;
 
   let control = true;
-  console.log(form)
   if (!form[0].children[0].children[2].value.match(stringRegex) || form[0].children[0].children[2].value.length < 2) {
     document.getElementById("firstNameErrorMsg").innerText = "Veuillez entrer 2 charactères ou plus pour le champ du prénom";
+    firstNameErrorMsg.classList.add("error")
     control = false;
   } else {
     document.getElementById("firstNameErrorMsg").innerText = "";
@@ -94,12 +92,14 @@ function validationregex(form) {
 
   if (!form[0].children[1].children[2].value.match(stringRegex) || form[0].children[1].children[2].value.length < 2) {
     document.getElementById("lastNameErrorMsg").innerText = "Veuillez entrer 2 charactères ou plus pour le champ du nom";
+    lastNameErrorMsg.classList.add("error")
     control = false;
   } else {
     document.getElementById("lastNameErrorMsg").innerText = "";
   }
   if (!form[0].children[2].children[2].value.match(emailRegex)) {
     document.getElementById("emailErrorMsg").innerText = "Veuillez entrer un email correct";
+    emailErrorMsg.classList.add("error")
     control = false;
   } else {
     document.getElementById("emailErrorMsg").innerText = "";
@@ -107,6 +107,7 @@ function validationregex(form) {
 
   if (!form[0].children[3].children[2].value.match(date_regex)) {
     document.getElementById("birthdateErrorMsg").innerText = "Entrez une date de naissance valide.";
+    birthdateErrorMsg.classList.add("error")
     control = false;
   } else {
     document.getElementById("birthdateErrorMsg").innerText = "";
@@ -114,13 +115,14 @@ function validationregex(form) {
 
   if (form[0].children[4].children[2].value === "") {
     document.getElementById("quantityErrorMsg").innerText = "Veuillez entrer le nombre de tournois participé.";
+    quantityErrorMsg.classList.add("error")
     control = false;
   } else {
     document.getElementById("quantityErrorMsg").innerText = "";
   }
-  console.log(form[0])
   if (!form[0].children[8].children[0].checked) {
     document.getElementById("CheckboxConditionErrorMsg").innerText = "Veuillez accepté les conditions d'utilisation.";
+    CheckboxConditionErrorMsg.classList.add("error")
     control = false;
   } else {
     document.getElementById("CheckboxConditionErrorMsg").innerText = "";
@@ -129,7 +131,6 @@ function validationregex(form) {
   let compteurlocation = 0;
 
   for (let i = 1; i < document.getElementById('containerlocation').getElementsByTagName('input').length + 1; i++) {
-    console.log("lalaaa")
     let location = document.getElementById("location" + i);
     if (location.checked) {
       compteurlocation++;
@@ -140,6 +141,7 @@ function validationregex(form) {
     document.getElementById("CheckboxVilleErrorMsg").innerText = "";
   } else {
     document.getElementById("CheckboxVilleErrorMsg").innerText = "Veuillez selectionné au moins une options.";
+    CheckboxVilleErrorMsg.classList.add("error")
     control = false;
   }
 
